90abb16eb92151ab207f743b9e103082f5faadcb
Dante <m.hussain@wsu.edu>
2020-09-08 20:22:32 -0700

    Files added

--- Task_3.c
+++ Task_3.c
@@ -11,12 +11,58 @@ int main(){
     
     fclose(fp);
     */
+   //-------------------
+   /*
     FILE *fp;
     fp = fopen("test.txt", "r");
     char buff[255];
+    fscanf(fp, "%s", buff);
+    printf("0: %s\n", buff);
     fgets(buff, 255, fp);
     printf("1: %s", buff);
     fgets(buff, 255, fp);
     printf("2: %s", buff);
+    fclose(fp);
     return 0;
+    */
+   FILE *fp;
+   fp = fopen("array.txt", "w+");
+   int i;
+   for (i=33;i<100;i++){
+       fputc(i,fp);
+   }
+   fclose(fp);
+   FILE *fp1;
+   fp1 = fopen("array.txt", "r");
+   int j;
+   int k;
+   int arr[68];
+   for (j=0;j<67;j++){
+       k= fgetc(fp1);
+       arr[j] = k;
+       printf("%d ",k);
+
+   }
+   printf("\nPlease print a specific number: \n");
+   printf("%d",arr[3]);
+   printf("\naverage num of the array\n");
+   int sum= 0;
+   for (int r=0;r<67;r++){
+       sum = sum +arr[r];
+   }
+   sum = sum/68;
+   printf("%d",sum);
+   int max = arr[0];
+   printf("\nMaxinum Number: %d %d", max, arr[6]);
+   for (int s= 0;s<67;s++){
+       
+       if (arr[s]> max){
+           max = arr[s];
+       }
+
+
+   }
+   printf("\nMaxinum Number: %d", max);
+   fclose(fp);
+   return 0;
 }
---
+++ string.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main( int argc, char**argv){
+
+    char arr[]= "apple";
+    printf("%s",arr);
+    return 0;
+}
